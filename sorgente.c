bb93c120d98b0d0ab88abee0afd626790101920b
natolo <10491192@polimi.it>
2020-08-30 20:38:43 +0200

    added undo/redo fix, free, to be tested

--- sorgente.c
+++ sorgente.c
@@ -27,17 +27,25 @@ void estraiUno(char* input,const char* delim, int* primary){//OK
     primary[0] = atoi(token);
 }
 
-void printer(int* primary, int* secondary, int* cmd, struct row*** versioni, int* countRow){
+void printer(int* primary, int* secondary, int* cmd, struct row*** versioni, int* rowDim){ //TODO va tenuto aggiornato countRow
     short dist;
     short out=0;
-    if(secondary[0]>countRow[0]) {
-        dist = countRow[0] - primary[0] + 1;
-        out = secondary[0] - countRow[0];
+    if(cmd[0]==-1){
+        out = secondary[0] - primary[0] +1;
+        while(out>0){
+            printf(".\n");
+            out--;
+        }
+        return;  
+    }
+    if(secondary[0]>rowDim[cmd[0]]) {
+        dist = rowDim[cmd[0]] - primary[0] + 1;
+        out = secondary[0] - rowDim[cmd[0]];
     }
     else dist = secondary[0] - primary[0] + 1;
-    if(countRow[0]!=0){
+    if((rowDim[cmd[0]]!=0)&&(cmd[0]!=-1)){
         for(short i=0; i<dist;i++){
-            printf("%s", versioni[cmd[0]][primary[0]-1+i]->stringa);
+                printf("%s", versioni[cmd[0]][primary[0]-1+i]->stringa);
         }
     }
     while(out>0){
@@ -46,7 +54,7 @@ void printer(int* primary, int* secondary, int* cmd, struct row*** versioni, int
     }
 }
 
-struct row* changer(int* primary, int* secondary, int* cmd, struct row* tail, struct row*** versioni, int* sizeVersioni, int* countRow){ 
+struct row* changer(int* primary, int* secondary, int* cmd, struct row* tail, struct row*** versioni, int* sizeVersioni, int* countRow, int* rowDim){ 
     struct row* new;
     cmd[0]++;
     short dist = secondary[0] - primary[0] + 1;
@@ -60,18 +68,29 @@ struct row* changer(int* primary, int* secondary, int* cmd, struct row* tail, st
         if(cmd[0]==sizeVersioni[0]){
             sizeVersioni[0] =  sizeVersioni[0]*2;
             versioni = (struct row***) realloc(versioni, sizeof(struct row**)*sizeVersioni[0]);
+            rowDim = (int*) realloc(rowDim, sizeof(int)*sizeVersioni[0]);
+            memset(rowDim,0,sizeVersioni[0]);
         }
-        if(secondary[0]<=countRow[0]){
-            versioni[cmd[0]] = (struct row**) realloc(versioni[cmd[0]-1], sizeof(struct row*)*(countRow[0]));
+        if(secondary[0]<=rowDim[cmd[0]-1]){
+            versioni[cmd[0]] = (struct row**) malloc(sizeof(struct row*)*(rowDim[cmd[0]-1]));
+            rowDim[cmd[0]] = rowDim[cmd[0]-1];
+            for(short i=0; i<primary[0];i++){
+                versioni[cmd[0]][i] = versioni[cmd[0]-1][i];
+            }
         }
         else{
-            versioni[cmd[0]] = (struct row**) realloc(versioni[cmd[0]-1], sizeof(struct row*)*secondary[0]);
+            rowDim[cmd[0]] = secondary[0];
+            versioni[cmd[0]] = (struct row**) malloc(sizeof(struct row*)*(rowDim[cmd[0]]));
+            for(short i=0; i<primary[0];i++){
+                versioni[cmd[0]][i] = versioni[cmd[0]-1][i];
+            }
             countRow[0]=secondary[0];
         }        
     }
     else{
         versioni[0] = (struct row**) malloc(sizeof(struct row*)*dist);
         countRow[0] = secondary[0];
+        rowDim[0] = countRow[0];
     }
     for(short i=0; i<dist;i++){
         //creo nuova riga e la metto in coda, nuova riga ora è la coda.
@@ -88,38 +107,42 @@ struct row* changer(int* primary, int* secondary, int* cmd, struct row* tail, st
     return new;
 }
 
-void deleter(int* primary, int* secondary, int* cmd, struct row*** versioni, int* sizeVersioni, int* countRow){
+void deleter(int* primary, int* secondary, int* cmd, struct row*** versioni, int* sizeVersioni, int* countRow, int* rowDim){
     cmd[0]++;
     short dist;
     if(countRow[0]==0){ //comandi successivi sono del oppure ho già cancellato tutte le righe
         if(cmd[0]==sizeVersioni[0]){
             sizeVersioni[0] =  sizeVersioni[0]*2;
             versioni = (struct row***) realloc(versioni, sizeof(struct row**)*sizeVersioni[0]);
+            rowDim = (int*) realloc(rowDim, sizeof(int)*sizeVersioni[0]);
+            memset(rowDim,0,sizeVersioni[0]);
         }
         versioni[cmd[0]] = NULL;
         return;        
     } 
-    if(primary[0]>countRow[0]){ //se la del è su righe non presenti 
-        versioni[cmd[0]] = (struct row **) malloc(sizeof(struct row *)* countRow[0]);
-        for(short i=0; i<countRow[0];i++){
+    if(primary[0]>rowDim[cmd[0]-1]){ //se la del è su righe non presenti 
+        rowDim[cmd[0]] = rowDim[cmd[0]-1];
+        versioni[cmd[0]] = (struct row **) malloc(sizeof(struct row *)*rowDim[cmd[0]]);
+        for(short i=0; i<rowDim[cmd[0]];i++){
             versioni[cmd[0]][i] = versioni[cmd[0]-1][i];
         }
         return;
     }
-    if(secondary[0]>countRow[0]) {
-        secondary[0] = countRow[0];
-    }
+    if(secondary[0]>rowDim[cmd[0]]) secondary[0] = rowDim[cmd[0]-1];
     dist = secondary[0] - primary[0] + 1;
     if(cmd[0]!=0){
         if(cmd[0]==sizeVersioni[0]){
             sizeVersioni[0] =  sizeVersioni[0]*2;
             versioni = (struct row***) realloc(versioni, sizeof(struct row**)*sizeVersioni[0]);
+            rowDim = (int*) realloc(rowDim, sizeof(int)*sizeVersioni[0]);
+            memset(rowDim,0,sizeVersioni[0]);           
         }
-        countRow[0]=countRow[0]-dist;   
-        versioni[cmd[0]] = (struct row**) malloc(sizeof(struct row*)*(countRow[0]));
+        countRow[0]=countRow[0]-dist;
+        rowDim[cmd[0]]= countRow[0];   
+        versioni[cmd[0]] = (struct row**) malloc(sizeof(struct row*)*(rowDim[cmd[0]]));
         //for dove copio solo i puntatori che mi interessano
         short shift = 1;
-        for(short i=0; i<countRow[0];i++){
+        for(short i=0; i<rowDim[cmd[0]];i++){
             if(i<primary[0]-1){
                 versioni[cmd[0]][i] = versioni[cmd[0]-1][i];
             }
@@ -131,21 +154,20 @@ void deleter(int* primary, int* secondary, int* cmd, struct row*** versioni, int
     }
 }
 
-void undo(int* actual, int* virtual){
-    if(virtual[0]>=0) actual[0] = virtual[0];
-    else actual[0] = -1;
-    return;
-}
 
-void redo(int* actual, int* virtual, int* max){
-    if(virtual[0]>=max[0]) actual[0] = max[0];
-    else actual[0] = virtual[0];
-    return;
+struct row* libera(int* cmd, struct row* tail, struct row*** versioni){
+    struct row* tmp = tail->prev;
+    while(tail->comando>cmd[0]){
+        if(tmp->comando<tail->comando){
+            free(versioni[tail->comando]);
+        }  
+        free(tail->stringa);
+        free(tail);
+        tail = tmp;
+        tmp = tmp->prev;       
+    }
+    return tail;
 }
-
-//struct row* libera(){
-//    return;
-//}
 
 int main () {
     
@@ -175,6 +197,11 @@ int main () {
     struct row*** versioni = (struct row***) malloc(sizeof(struct row**)*sizeVersion[0]);
 
 
+    //array dimensioni
+    int* rowDim;
+    rowDim = (int*) malloc(sizeVersion[0]*sizeof(int));
+    
+
     int primary[1]; //modifico questi indici nella matrice delle versioni
     int secondary[1];
 
@@ -184,26 +211,30 @@ int main () {
     int max[1];
     max[0]=countcmd[0];
 
+    
+
     const char delim[] = ", c d p u r"; 
     fgets(input, sizeof(input), stdin);
     while(strcmp(input, "q\n")!=0){
         cmd[0]=input[strlen(input)-2];
         while((cmd[0]=='u')||(cmd[0]=='r')){
-            if(!u) break;
+            //if(!u) break;
             estraiUno(input, delim, primary);
-            if(cmd[0]=='u') virtual[0]=virtual[0]+primary[0];
-            else virtual[0]=virtual[0]-primary[0];
+            if(cmd[0]=='u') virtual[0]=virtual[0]-primary[0];
+            else virtual[0]=virtual[0]+primary[0];
             u=true;
             fgets(input, sizeof(char)*INPUTLENGHT, stdin);
+            cmd[0]=input[strlen(input)-2];
         }
         if(cmd[0]=='c'){ //change
             if(u){
-                undo(countcmd, virtual);
-                //tail = libera();
+                if(virtual[0]>=0) countcmd[0] = virtual[0];
+                else countcmd[0] = -1;
+                tail = libera(countcmd, tail, versioni);
                 u = false;
             }
             estraiDue(input, delim, primary, secondary);
-            tail = changer(primary, secondary, countcmd, tail, versioni, sizeVersion, countRow);
+            tail = changer(primary, secondary, countcmd, tail, versioni, sizeVersion, countRow, rowDim);
             fgets(dot, sizeof(char)*2, stdin); //termino con punto, solo per scorrere gli input   
             virtual[0] = countcmd[0];
             max[0] =  countcmd[0];
@@ -211,24 +242,27 @@ int main () {
         }
         else if (cmd[0]=='d'){ //delete
             if(u){
-                undo(countcmd, virtual);
+                if(virtual[0]>=0) countcmd[0] = virtual[0];
+                else countcmd[0] = -1;
                 //tail = libera(); NON SERVE
                 u = false;
             }
             estraiDue(input, delim, primary, secondary);
-            deleter(primary, secondary, countcmd, versioni, sizeVersion, countRow);
+            deleter(primary, secondary, countcmd, versioni, sizeVersion, countRow, rowDim);
             virtual[0] = countcmd[0]; 
             max[0] =  countcmd[0];
         }
         else if (cmd[0]=='p'){ //print
             if(virtual[0]<countcmd[0]){
-                undo(countcmd, virtual);
+                if(virtual[0]>=0) countcmd[0] = virtual[0];
+                else countcmd[0] = -1;
             }
             else if (virtual[0]>countcmd[0]){
-                redo(countcmd, virtual, max);//al massimo torno a countcmd
+                if(virtual[0]>=max[0]) countcmd[0] = max[0];
+                else countcmd[0] = virtual[0];
             }
             estraiDue(input, delim, primary, secondary);
-            printer(primary, secondary, countcmd, versioni, countRow);
+            printer(primary, secondary, countcmd, versioni, rowDim);
             //printf("Print finita\n");
         }
         //printf("%d %c\n", countcmd[0], lstcmds[countcmd[0]].cmd[0]);
